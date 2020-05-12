4763d74921e95d4e8bb2ff44b7bbbbf4564225e0
BP@ <ansemsnobody@gmail.com>
2020-05-12 18:46:35 -0400

    Moved scenes in to sub folder, started menu system

---
+++ Assets/BPA-Scripts/MenuSystem/SimpleMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMenu : MonoBehaviour
+{
+    float slowWait = 0.5f;
+    float fastWait = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
