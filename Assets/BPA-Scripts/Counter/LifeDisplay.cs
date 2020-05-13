0c8738ca803e0714685db490b4f8f4b2fee344af
BP@ <ansemsnobody@gmail.com>
2020-05-13 18:58:29 -0400

    Comments moved around

--- Assets/BPA-Scripts/Counter/LifeDisplay.cs
+++ Assets/BPA-Scripts/Counter/LifeDisplay.cs
@@ -12,7 +12,6 @@ public class LifeDisplay : MonoBehaviour
     public GameObject[] LifeObjects;
     Action myAction;
     int val = 0;
-    // Start is called before the first frame update
     void Start()
     {
         myAction = ()=> UpdateDisplay();
@@ -33,5 +32,4 @@ public class LifeDisplay : MonoBehaviour
             }
         }
     }
-   
 }
