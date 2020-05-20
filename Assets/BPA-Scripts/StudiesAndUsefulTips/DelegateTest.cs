458af8b5de10d7040d3a8d64227e4d61bc2f2c3c
BP@ <ansemsnobody@gmail.com>
2020-05-19 20:46:32 -0400

    Note on how to use multiple Actions

---
+++ Assets/BPA-Scripts/StudiesAndUsefulTips/DelegateTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateTest : MonoBehaviour
+{
+    /*
+     * How to use multiple actions
+     */
+
+    public Action events;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        events += DoThing;
+        events += DoThing2;
+    }
+    void DoThing()
+    {
+        Debug.Log("MOT");
+    }
+    void DoThing2()
+    {
+        Debug.Log("1111");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        events();
+    }
+}
